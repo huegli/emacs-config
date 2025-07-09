fd71e30d3bac0e8f90b7bb803f0a6705a79c0a18
Nikolai Schlegel <nschlegel@apple.com>
2025-07-08 22:27:25 -0700

    Work version

---
+++ work-emacs-config/early-init.el
@@ -0,0 +1,33 @@
+;;; Tweak the looks of Emacs
+
+(menu-bar-mode 1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(let ((mono-spaced-font "MonoLisa")
+      (proportionately-spaced-font "Atkinson Hyperlegible Next"))
+  (set-face-attribute 'default nil :family mono-spaced-font :height 140)
+  (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
+  (set-face-attribute 'variable-pitch nil :family proportionately-spaced-font :height 1.1))
+
+;; https://emacsredux.com/blog/2025/03/28/speed-up-emacs-startup-by-tweaking-the-gc-settings/
+
+;; Temporarily increase GC threshold during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Restore to normal value after startup (e.g. 50MB)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
+
+;; Load themes early to avoid flickering during startup (you need a built-in theme, though)
+(load-theme 'modus-operandi t)
+
+(setq inhibit-startup-screen t)
+
+;; tweak native compilation settings
+(setq native-comp-speed 2)
+
+;; From Mastering Emacs, https://www.masteringemacs.org/article/spotlight-use-package-a-declarative-configuration-tool
+
+;; need to set this before fist use-package
+(setq use-package-enable-imenu-support t)
---
+++ work-emacs-config/init.el
@@ -0,0 +1,185 @@
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
+
+;;; Set up the package manager
+
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(add-to-list 'display-buffer-alist
+             '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
+               (display-buffer-no-window)
+               (allow-no-window . t)))
+
+;;; Mac OSX Stuff
+(when (eq system-type 'darwin)
+  
+  ;; Prevent macOS from creating new frames
+  (setq ns-pop-up-frames nil)
+  
+  ;; Improve font rendering
+  (setq ns-use-thin-smoothing t))
+
+;;; Functions
+
+(defun prot/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+;;; Basic behaviour
+
+(use-package emacs
+  :ensure nil
+  :bind
+  (("C-g" . #'prot/keyboard-quit-dwim)
+   ("M-i" . imenu)
+   ("M-o" . other-window)
+   ("C-x C-b" . ibuffer))
+  :config
+  (setq-default indent-tabs-mode nil)
+  :custom
+  ;; Smooth scrolling
+  (pixel-scroll-precision-mode 1)
+  ;; Better default modes
+  (electric-pair-mode t)
+  (show-paren-mode 1)
+  (save-place-mode t)
+  (savehist-mode t)
+  (recentf-mode t)
+  (global-auto-revert-mode t)
+  ;; imenu configuration: https://www.gnu.org/software/emacs/manual/html_node/emacs/Imenu.html
+  (imenu-flatten 'group)
+  (imenu-auto-rescan t)
+  (imenu-auto-rescan-maxout (* 1024 1024))
+  (imenu-max-index-time 2))
+
+(use-package emacs-lisp-mode
+  :ensure nil
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'imenu-add-menubar-index))
+
+(use-package delsel
+  :ensure nil
+  :hook (after-init . delete-selection-mode))
+
+(use-package nerd-icons
+  :ensure t)
+
+(use-package nerd-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package savehist
+  :ensure nil ; it is built-in
+  :hook (after-init . savehist-mode))
+
+;;; The file manager (Dired)
+
+(use-package dired
+  :ensure nil
+  :commands (dired)
+  :hook
+  ((dired-mode . dired-hide-details-mode)
+   (dired-mode . hl-line-mode))
+  :config
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'always)
+  (setq delete-by-moving-to-trash t)
+  (setq dired-dwim-target t))
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>" . dired-subtree-toggle)
+    ("TAB" . dired-subtree-toggle)
+    ("<backtab>" . dired-subtree-remove)
+    ("S-TAB" . dired-subtree-remove))
+  :config
+  (setq dired-subtree-use-backgrounds nil))
+
+(use-package trashed
+  :ensure t
+  :commands (trashed)
+  :config
+  (setq trashed-action-confirmer 'y-or-n-p)
+  (setq trashed-use-header-line t)
+  (setq trashed-sort-key '("Date deleted" . t))
+  (setq trashed-date-format "%Y-%m-%d %H:%M:%S"))
+
+(use-package which-key
+  :ensure nil
+  :config
+  (which-key-mode))
+
+(use-package org
+  :ensure nil
+  :config
+  (add-hook 'org-mode-hook #'imenu-add-menubar-index))
+
+(use-package denote
+  :ensure t
+  :bind
+  (("C-c d n" . denote-open-or-create)
+   ("C-c d d" . denote-sort-dired)
+   ("C-c d r" . denote-rename-file))
+  :custom
+  ((denote-directory "~/Library/Mobile Documents/com~apple~icloud~applecorporate/Documents/Denote")
+   (denote-file-type 'org)
+   (denote-known-keywords '("management" "oneonone" "subra" "seb" "jose" "alex" "prashanth" "nguyen" "franz" "najib" "subhasis" "subra" "navid" "daniel"))
+   (denote-date-prompt-use-org-read-date t)
+   (denote-dired-directories '("~/Library/Mobile Documents/com~apple~icloud~applecorporate/Documents/Denote" "~/Desktop" "~/Downloads"))
+   (denote-dired-directories-include-subdirectories t))
+  :config
+  (denote-rename-buffer-mode 1))
+
+(use-package denote-org
+  :ensure t
+  :bind
+  (:map org-mode-map
+        ("C-c l d l" . denote-org-link-to-heading)))
+
+(use-package denote-journal
+  :ensure t
+  ;; Bind those to some key for your convenience.
+  :bind
+  (("C-c d j" . denote-journal-new-or-existing-entry)
+   ("C-c d J" . denote-journal-link-or-create-entry))
+  :hook (calendar-mode . denote-journal-calendar-mode)
+  :config
+  ;; Use the "journal" subdirectory of the `denote-directory'.  Set this
+  ;; to nil to use the `denote-directory' instead.
+  (setq denote-journal-directory
+        (expand-file-name "journal" denote-directory))
+  ;; Default keyword for new journal entries. It can also be a list of
+  ;; strings.
+  (setq denote-journal-keyword "journal")
+  ;; Read the doc string of `denote-journal-title-format'.
+  (setq denote-journal-title-format 'day-date-month-year))
+
+
+
