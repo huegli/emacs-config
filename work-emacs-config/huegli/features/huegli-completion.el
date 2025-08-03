fb3ddea3e6b416bd7c077985a7ffe8df5d9af5ac
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2025-08-03 15:13:57 -0700

    Align updates with work config

--- work-emacs-config/huegli/features/huegli-completion.el
+++ work-emacs-config/huegli/features/huegli-completion.el
@@ -6,8 +6,32 @@
 
 (use-package icomplete
   :ensure nil
-  :custom
-  (icomplete-vertical-mode t))
+  :bind (:map icomplete-minibuffer-map
+              ("C-n" . icomplete-forward-completions)
+              ("C-p" . icomplete-backward-completions)
+              ("C-v" . icomplete-vertical-mode) ;; toggle vertical
+              ("RET" . icomplete-force-complete-and-exit)
+              ("C-j" . exit-minibuffer)) ;; So we can exit commands like `multi-file-replace-regexp-as-diff'
+  :hook
+  (after-init-hook . (lambda ()
+                       (fido-mode -1)
+                       (icomplete-vertical-mode 1)))
+  :config
+  (setq icomplete-delay-completions-threshold 0)
+  (setq icomplete-compute-delay 0)
+  (setq icomplete-show-matches-on-no-input t)
+  (setq icomplete-hide-common-prefix nil)
+  (setq icomplete-prospects-height 10)
+  (setq icomplete-separator " . ")
+  (setq icomplete-with-completion-tables t)
+  (setq icomplete-in-buffer t)
+  (setq icomplete-max-delay-chars 0)
+  (setq icomplete-scroll t)
+
+  (if icomplete-in-buffer
+      (advice-add 'completion-at-point
+                  :after #'minibuffer-hide-completions)))
+
 
 (use-package orderless
   :custom
