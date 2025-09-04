2fe089988ecf6daf1f875517aa8c78ad176980cf
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2025-09-04 09:31:18 -0700

    Further vterm configuration for fish etc

--- huegli/features/huegli-terminal.el
+++ huegli/features/huegli-terminal.el
@@ -18,8 +18,11 @@
   (eat-enable-mouse t))
 
 (use-package vterm
-  :ensure t
-  :defer t)
+  :custom
+  (vterm-shell "/opt/homebrew/bin/fish")
+  (vterm-kill-buffer-on-exit t)
+  (vterm-tramp-shells '(("ssh" "/usr/bin/tcsh"))) ; For Apple CE VM's
+  (vterm-always-compile-module t))
 
 (provide 'huegli-terminal)
 ;;; huegli-terminal.el ends here
