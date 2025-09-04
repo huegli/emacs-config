1f92f9ddbe4752f3e47573b0b4e91e7961bac14c
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2025-09-04 09:31:55 -0700

    Simplify detached for now

--- huegli/features/huegli-remote.el
+++ huegli/features/huegli-remote.el
@@ -12,23 +12,23 @@
   :init
   (detached-init)
   :bind (;; Replace `async-shell-command' with `detached-shell-command'
-         ([remap async-shell-command] . detached-shell-command)
+         ;;([remap async-shell-command] . detached-shell-command)
          ;; Replace `compile' with `detached-compile'
-         ([remap compile] . detached-compile)
-         ([remap recompile] . detached-compile-recompile)
+         ;;([remap compile] . detached-compile)
+         ;;([remap recompile] . detached-compile-recompile)
          ;; Replace built in completion of sessions with `consult'
          ;; ([remap detached-open-session] . detached-consult-session)
-         )
-  :custom
-  ((show-output-on-attach t)
-   (detached-terminal-data-command system-type)
-   (detached-notification-function #'detached-extra-alert-notification)))
+         ))
+:custom
+;;((show-output-on-attach t)
+;; (detached-terminal-data-command system-type)
+;; (detached-notification-function #'detached-extra-alert-notification)))
 
 ;; https://github.com/jwiegley/alert
-(use-package alert
-  :ensure t
-  :custom
-  (alert-default-style 'notifier)) ;; requires terminal-notifier to be installed
+;;(use-package alert
+;;  :ensure t
+;;  :custom
+;;  (alert-default-style 'notifier)) ;; requires terminal-notifier to be installed
 
 (provide 'huegli-remote)
 ;;; huegli-remote.el ends here
