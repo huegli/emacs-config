7cccada9dbc2132442eeea1f6c6ac051fceb4757
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2025-10-18 11:06:34 -0700

    Using agent-shell instead of Claude Code directly

--- huegli/features/huegli-ai.el
+++ huegli/features/huegli-ai.el
@@ -32,14 +32,35 @@
               google/gemini-2.5-flash))
   (setq gptel-backend (gptel-get-backend "Perplexity")))
 
-(use-package claude-code
-  :ensure t
-  :defer t
-  :commands (claude-code-mode)
-  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+;; (use-package claude-code
+;;   :ensure t
+;;   :defer t
+;;   :commands (claude-code-mode)
+;;   :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+;;   :config
+;;   (claude-code-mode)
+;;   :bind-keymap ("C-c c" . claude-code-command-map))
+
+;; (use-package claude-code-ide
+;;   :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :rev :newest)
+;;   :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
+;;   :config
+;;   (claude-code-ide-emacs-tools-setup)) ; Optionally enable Emacs MCP tools
+
+(use-package shell-maker
+  :ensure t)
+
+(use-package acp
+  :vc (:url "https://github.com/xenodium/acp.el"))
+
+(use-package agent-shell
+  :vc (:url "https://github.com/xenodium/agent-shell")
   :config
-  (claude-code-mode)
-  :bind-keymap ("C-c c" . claude-code-command-map))
+  (setq agent-shell-google-authentication
+        (agent-shell-google-make-authentication :login t))
+  (setq agent-shell-anthropic-authentication
+        (agent-shell-anthropic-make-authentication :login t)))
+
 
 (provide 'huegli-ai)
 ;;; huegli-ai.el ends here
